3f9024d1fbb12da3a9821f26f735f838b5b919d6
ARahmanLskit <arahman@lskit.com>
2023-08-09 11:58:39 +0600

    js file delete. and convert ts file to js file

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var cors_1 = require("cors");
-var db_1 = require("./api/db");
-var app = (0, express_1.default)();
-var PORT = 8000;
-app.use((0, cors_1.default)());
+import express from 'express';
+import cors from 'cors';
+
+import { db } from './api/db';
+import { User } from './interfaces/user.interface';
+
+const app = express();
+const PORT = 8000;
+
+app.use(cors());
+
 /**
  * Nothing on root, so return a 404
  */
-app.get('/', function (req, res) { return res.send("this is native backend"); });
+app.get('/', (req, res) => res.send("this is native backend"));
+
 /**
  * Return list of users as application/json
  * Publish API as V1 to be able to support legacy apps after breaking changes of the API
  */
-app.get('/api/v1/users', function (req, res) {
-    db_1.db.getUsers().then(function (users) {
-        res.send(users);
-    });
-});
-app.listen(PORT, function () {
-    console.log("[backend]: Server is running on https://localhost:".concat(PORT));
+app.get('/api/v1/users', (req, res) => {
+  db.getUsers().then((users) => {
+    res.send(users);
+  });
+})
+
+app.listen(PORT, () => {
+  console.log(`[backend]: Server is running on https://localhost:${PORT}`);
 });
